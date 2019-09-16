50e9ac4c5724cadd5511589218ab1d6c67d2a114
Doron <lisbona107@gmail.com>
2019-09-16 15:08:09 +0300

    all project, see the readMe file

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetSavedRepositoryIds()
+        {
+            var allRepoString = HttpContext.Session.GetString("allRepo");
+            var list = new List<int>();
+            if (allRepoString != null)
+            {
+                List<githubRepositoryModel> allRepoSaved = JsonConvert.DeserializeObject<List<githubRepositoryModel>>(allRepoString);
+                foreach(githubRepositoryModel repo in allRepoSaved)
+                {
+                    list.Add(repo.Id);
+                }
+            }
+            return Json(list);
+          
+        }
+        public IActionResult ShowSavedGithubRepository()
+        {
+            int amountGithubRepositorySaved = 0;
+            var allRepoString = HttpContext.Session.GetString("allRepo");
+            string strAmountGithubRepositorySaved = HttpContext.Session.GetString("amountSavedRepository");
+
+            if(!(strAmountGithubRepositorySaved == null))
+            {
+                amountGithubRepositorySaved = int.Parse(strAmountGithubRepositorySaved);
+            }
+
+            if (amountGithubRepositorySaved == 0)
+            {
+                ViewBag.errorMessage = "You don't have any repository saved";
+            }
+            else
+            {
+                List<githubRepositoryModel> allRepoSaved = JsonConvert.DeserializeObject<List<githubRepositoryModel>>(allRepoString);
+                ViewBag.allRepositoriesSaved = allRepoSaved;
+            }
+
+            ViewBag.amountRepositorySaved = amountGithubRepositorySaved;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult SaveFavoriteRepository(string repo)
+        {
+            var allRepoString = HttpContext.Session.GetString("allRepo");
+            var list = new List<githubRepositoryModel>();
+            githubRepositoryModel curentRepository = JsonConvert.DeserializeObject<githubRepositoryModel>(repo);
+            var checkedIfCurrentRepositoryExist = false; 
+            var test = HttpContext.Session.GetString("test");
+
+            if (allRepoString == null)
+            {
+                HttpContext.Session.SetString("amountSavedRepository", "1");
+            }
+            else
+            {
+                List<githubRepositoryModel> allRepositorySaved = JsonConvert.DeserializeObject<List<githubRepositoryModel>>(allRepoString);
+                foreach (githubRepositoryModel savedRepo in allRepositorySaved)
+                {
+                    if(curentRepository.Id == savedRepo.Id)
+                    {
+                        checkedIfCurrentRepositoryExist = true;
+                    }
+                    else // if is exist we deleted it
+                    {
+                        list.Add(savedRepo);
+
+                    }
+                }
+            }
+
+            if(checkedIfCurrentRepositoryExist == false)
+            {
+                list.Add(curentRepository);
+            }
+          
+            // update the amount od saved repository and the all the saved repository
+            HttpContext.Session.SetString("amountSavedRepository", list.Count.ToString());
+            HttpContext.Session.SetString("allRepo", JsonConvert.SerializeObject(list));
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,93 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const app = angular.module("myApp", []);
+
+app.controller("myCtrl", function ($scope, $http) {
+
+    let allRepository;
+    $scope.showRepository = false;
+
+    // return all github repository filter by user searched
+    $scope.searchGithubRepository = () => {
+        $("#overlay").fadeIn(300); // show the loading gif
+        
+
+        // get exist repository
+        $http({
+            method: "GET",
+            url: "Home/GetSavedRepositoryIds"
+        })
+            .then((response) => {
+                const savedRepositoriesIds = response.data;
+
+                callGithubApi(savedRepositoriesIds);
+            });
+    }
+
+    function callGithubApi(savedRepositoriesIds) {
+        let arrRepositories = new Array();
+        let isExistSavedRepository = false;
+        if (savedRepositoriesIds.length > 0) {
+            isExistSavedRepository = true;
+        }
+        $http({
+            method: "GET",
+            url: 'https://api.github.com/search/repositories?q=' + $scope.userSearch
+        })
+            .then(
+                function mySuccess(response) {
+                    allRepository = response.data.items;
+                    for (let i = 0; i < allRepository.length; i++) {
+
+                        let isSavedRepository = "false";
+                        if (isExistSavedRepository === true) {
+                            if (savedRepositoriesIds.includes(allRepository[i].id)) {
+                                isSavedRepository = "true";
+                            }
+                        }
+                        arrRepositories.push({
+                            "id": allRepository[i].id,
+                            "name": allRepository[i].name,
+                            "avatar": allRepository[i].owner.avatar_url,
+                            "isSavedRepository": isSavedRepository
+                        });
+                    }
+                    $scope.allGithubRepository = arrRepositories;
+
+                    setTimeout(() => {
+                        $("#overlay").fadeOut(300); // hide the loading gif
+                        $scope.showRepository = true;
+                    }, 500);
+                },
+                function myError(response) {
+                    $scope.errorMessage = response.statusText;
+                    $("#overlay").fadeOut(300); // hide the loading gif
+                    $scope.showRepository = true;
+                });   
+    }
+    // change the favorite icon
+    $scope.changeFavorite = ($event, repoPassed) => {
+        $event.target.classList.toggle("checked");
+        let repo = {
+            Id: repoPassed.id,
+            Name: repoPassed.name,
+            ImagePath: repoPassed.avatar
+        }
+
+        $http({
+            method: "post",
+            url: `Home/SaveFavoriteRepository?repo=${JSON.stringify(repo)}`
+
+        })
+        .then(function success(response) {
+        },
+        function error(error) {
+            console.log(error)
+        })
+    }
+});
+
+
